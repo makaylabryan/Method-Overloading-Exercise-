898c6d34013803797c4e7f4b77e6118d4641874e
Makayla Bryan <makaylabryan@gmail.com>
2023-03-24 18:47:48 -0400

    practice with method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,46 @@
 ﻿namespace MethodOverloading
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static int Add(int x, int y)
         {
-            Console.WriteLine("Hello, World!");
+            return x + y;
+        }
+        public static double Add(double a, double b)
+        { 
+            return a + b; 
+        }
+        public static string Add(int m, int n, bool bill)
+        {
+            var sum = m + n;
+            if (bill == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (bill == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else 
+            {
+                return $"{sum}";
+            }
+        }
+        public static void Main(string[] args)
+        {
+            var x = 16;
+            var y = 17;
+                var answer = Add(x, y);
+
+            var a = .0000001;
+            var b = .0000002;
+                var doubleAnswer = Add(a, b);
+
+            var billAnswer = Add(1000000000, 3, true);
+
+            Console.WriteLine($"int add: {answer}");
+            Console.WriteLine($"decimal add: {doubleAnswer}");
+            Console.WriteLine($"Your bill is {billAnswer}");
         }
     }
 }
